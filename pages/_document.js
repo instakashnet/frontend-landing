42bc71851c6479cf7c640d98bc2a5acc1587f3f4
Roger Rengifo <thelea12@gmail.com>
2021-05-05 18:36:44 -0500

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,22 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='es' class='notranslate' translate='no'>
-        <Head />
+      <Html lang='es' className='notranslate' translate='no'>
+        <Head>
+          <scrip async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+          <script
+            dangerouslySetInnerHtml={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+  `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
