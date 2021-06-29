0011a4a588feefcd0e7c7f753016942cb85b2d62
Roger Rengifo <thelea12@gmail.com>
2021-06-28 21:44:07 -0500

    info modal

---
+++ components/UI/Modal.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import { XCircle } from "react-feather";
+
+Modal.setAppElement("div");
+
+export const BaseModal = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsOpen(true), 500);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <Modal isOpen={isOpen} onRequestClose={closeModal} className="custom-modal">
+      <div className="modal-content">
+        <button onClick={closeModal} className="close-modal">
+          <XCircle size={30} />
+        </button>
+        {children}
+      </div>
+      <div className="modal-footer">
+        <img src="/images/logo.svg" alt="Instakash" />
+      </div>
+    </Modal>
+  );
+};
