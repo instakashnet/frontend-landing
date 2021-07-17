927c071e72d070f438d4cbe9f1d540374ffd4a9e
Roger Rengifo <thelea12@gmail.com>
2021-07-17 13:07:46 -0500

    adding github actions yaml

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,10 @@
-import Link from 'next/link';
-import axios from 'axios';
-import moment from 'moment';
+import Link from "next/link";
+import axios from "axios";
+import moment from "moment";
 
-import BlogItem from '../components/UI/blog/BlogItem';
+import BlogItem from "../components/UI/blog/BlogItem";
 
-import styles from '../styles/Blog.module.scss';
+import styles from "../styles/Blog.module.scss";
 
 const { CONTENT_API_KEY, BLOG_API_URL } = process.env;
 
@@ -15,7 +15,7 @@ export const getStaticProps = async ({ params }) => {
   };
 };
 
-moment.locale('es');
+moment.locale("es");
 
 const Blog = ({ posts, meta }) => {
   const recentPost = posts[0];
@@ -25,41 +25,41 @@ const Blog = ({ posts, meta }) => {
   copiedPosts.pop();
 
   return (
-    <section className='container section-wrapper'>
+    <section className="container section-wrapper">
       <div className={styles.recentPost}>
         <img src={recentPost.feature_image} alt={recentPost.title} />
         <div className={styles.postInfo}>
-          <Link href='/post/[slug]' as={`/post/${recentPost.slug}`}>
+          <Link href="/post/[slug]" as={`/post/${recentPost.slug}`}>
             <a>
               <h1>{recentPost.title}</h1>
             </a>
           </Link>
-          <span>{moment(recentPost.published_at).format('DD MMM [del] YYYY')}</span>
+          <span>{moment(recentPost.published_at).format("DD MMM [del] YYYY")}</span>
           <p>{recentPost.custom_excerpt}</p>
-          <Link href='/post/[slug]' as={`/post/${recentPost.slug}`}>
+          <Link href="/post/[slug]" as={`/post/${recentPost.slug}`}>
             <a>Leer más</a>
           </Link>
         </div>
         <div className={styles.postOverlay} />
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-3'>
-        <div className='md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-5'>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-5">
           {copiedPosts.map((post) => (
             <BlogItem key={post.id} post={post} />
           ))}
         </div>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-3'>
-        <BlogItem post={lastPagePost} className='col-span-2' />
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <BlogItem post={lastPagePost} className="col-span-2" />
       </div>
       <div className={styles.blogNav}>
-        <Link href='/#steps'>
-          <a>¿Cómo funciona?</a>
+        <Link href="/#steps">
+          <a>¿Cómo funciona?!</a>
         </Link>
-        <Link href='/nosotros'>
+        <Link href="/nosotros">
           <a>Nosotros</a>
         </Link>
-        <Link href='/contacto'>
+        <Link href="/contacto">
           <a>Contacto</a>
         </Link>
       </div>
