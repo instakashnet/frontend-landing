495776c19823f2dd7d1b86ef38218e2dad2f32ca
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2021-11-29 20:30:32 -0500

    fixes in head pages title

--- pages/libro-de-reclamaciones.js
+++ pages/libro-de-reclamaciones.js
@@ -1,33 +1,34 @@
-import { useState } from 'react';
-import { useFormik } from 'formik';
-import emailjs from 'emailjs-com';
-import { reclamacionesValidation } from '../helpers/validations';
+import { useState } from "react";
+import { useFormik } from "formik";
+import emailjs from "emailjs-com";
+import Head from "next/head";
+import { reclamacionesValidation } from "../helpers/validations";
 
-import Input from '../components/UI/form/Input';
-import Select from '../components/UI/form/Select';
-import Textarea from '../components/UI/form/Textarea';
-import Button from '../components/UI/Button';
+import Input from "../components/UI/form/Input";
+import Select from "../components/UI/form/Select";
+import Textarea from "../components/UI/form/Textarea";
+import Button from "../components/UI/Button";
 
 const LibroReclamaciones = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [sent, setSent] = useState(false);
 
   const documentOptions = [
-    { value: 'DNI', label: 'DNI' },
-    { value: 'PTP', label: 'PTP' },
-    { value: 'CE', label: 'Carnet de extranjería' },
-    { value: 'Pasaporte', label: 'Pasaporte' },
+    { value: "DNI", label: "DNI" },
+    { value: "PTP", label: "PTP" },
+    { value: "CE", label: "Carnet de extranjería" },
+    { value: "Pasaporte", label: "Pasaporte" },
   ];
   const servicesOptions = [
-    { value: 'Cambios de divisas (compra)', label: 'Cambios de divisas (compra)' },
-    { value: 'Cambios de divisas (venta)', label: 'Cambios de divisas (venta)' },
+    { value: "Cambios de divisas (compra)", label: "Cambios de divisas (compra)" },
+    { value: "Cambios de divisas (venta)", label: "Cambios de divisas (venta)" },
   ];
 
   const onSubmit = async (values) => {
     setIsLoading(true);
 
     try {
-      const res = await emailjs.send('instakash_service', 'inquiry_template', values, 'user_HpWIiQWPXIgpMaV2NuEIB');
+      const res = await emailjs.send("instakash_service", "inquiry_template", values, "user_HpWIiQWPXIgpMaV2NuEIB");
       if (res.status === 200) {
         setSent(true);
       }
@@ -41,18 +42,18 @@ const LibroReclamaciones = () => {
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      fromEmail: '',
-      documentType: '',
-      documentNo: '',
-      companyName: '',
-      ruc: '',
-      phone: '',
-      service: '',
-      amount: '',
-      description: '',
-      formType: '',
+      firstName: "",
+      lastName: "",
+      fromEmail: "",
+      documentType: "",
+      documentNo: "",
+      companyName: "",
+      ruc: "",
+      phone: "",
+      service: "",
+      amount: "",
+      description: "",
+      formType: "",
     },
     onSubmit,
     validationSchema: reclamacionesValidation,
@@ -60,20 +61,23 @@ const LibroReclamaciones = () => {
 
   return (
     <>
+      <Head>
+        <title> Libro de reclamaciones | Instakash</title>
+      </Head>
       <section>
-        <div className='section-wrapper container px-3'>
-          <h1 className='text-center mt-16 md:mt-36 mb-4'>Libro de reclamaciones</h1>
+        <div className="section-wrapper container px-3">
+          <h1 className="text-center mt-16 md:mt-36 mb-4">Libro de reclamaciones</h1>
           <p>
             Gestionamos tus reclamos para poder procesarlos de forma inmediata. Sabemos lo importante que es tu tiempo y tu dinero por lo que atendemos cada caso según su prioridad
             y a la breveddad. Cambiamos la forma de operar y te damos la garantía de seguridad más alta.
           </p>
 
-          <form onSubmit={formik.handleSubmit} className='max-w-2xl mx-auto mt-16'>
-            <div className='grid md:grid-cols-2 gap-4'>
+          <form onSubmit={formik.handleSubmit} className="max-w-2xl mx-auto mt-16">
+            <div className="grid md:grid-cols-2 gap-4">
               <Input
-                type='text'
-                name='firstName'
-                placeholder='Ingresa tu nombre'
+                type="text"
+                name="firstName"
+                placeholder="Ingresa tu nombre"
                 value={formik.values.firstName}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -81,9 +85,9 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.firstName}
               />
               <Input
-                type='text'
-                name='lastName'
-                placeholder='Ingresa tu apellido'
+                type="text"
+                name="lastName"
+                placeholder="Ingresa tu apellido"
                 value={formik.values.lastName}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -91,11 +95,11 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.lastName}
               />
             </div>
-            <div className='grid md:grid-cols-2 gap-4'>
+            <div className="grid md:grid-cols-2 gap-4">
               <Input
-                type='email'
-                name='fromEmail'
-                placeholder='Correo electrónico'
+                type="email"
+                name="fromEmail"
+                placeholder="Correo electrónico"
                 value={formik.values.fromEmail}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -103,9 +107,9 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.fromEmail}
               />
               <Input
-                type='text'
-                name='phone'
-                placeholder='Teléfono'
+                type="text"
+                name="phone"
+                placeholder="Teléfono"
                 value={formik.values.phone}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -113,19 +117,19 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.phone}
               />
             </div>
-            <div className='grid md:grid-cols-2 gap-4'>
+            <div className="grid md:grid-cols-2 gap-4">
               <Select
-                name='documentType'
-                placeholder='Documento de identidad'
+                name="documentType"
+                placeholder="Documento de identidad"
                 options={documentOptions}
                 value={formik.values.documentType}
                 value={formik.values.documentType}
                 onChange={formik.handleChange}
               />
               <Input
-                type='text'
-                name='documentNo'
-                placeholder={`Nro. de ${formik.values.documentType ? formik.values.documentType : 'documento'}`}
+                type="text"
+                name="documentNo"
+                placeholder={`Nro. de ${formik.values.documentType ? formik.values.documentType : "documento"}`}
                 value={formik.values.documentNo}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -133,11 +137,11 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.documentNo}
               />
             </div>
-            <div className='grid md:grid-cols-2 gap-4'>
+            <div className="grid md:grid-cols-2 gap-4">
               <Input
-                type='text'
-                name='companyName'
-                placeholder='Nombre de empresa (si aplica)'
+                type="text"
+                name="companyName"
+                placeholder="Nombre de empresa (si aplica)"
                 value={formik.values.companyName}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -145,9 +149,9 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.companyName}
               />
               <Input
-                type='text'
-                name='ruc'
-                placeholder='RUC de la empresa (si aplica)'
+                type="text"
+                name="ruc"
+                placeholder="RUC de la empresa (si aplica)"
                 value={formik.values.ruc}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -156,19 +160,19 @@ const LibroReclamaciones = () => {
               />
             </div>
             <Select
-              name='service'
-              placeholder='¿Que servicio adquiriste?'
+              name="service"
+              placeholder="¿Que servicio adquiriste?"
               options={servicesOptions}
               value={formik.values.service}
               error={formik.errors.service}
               touched={formik.touched.service}
               onChange={formik.handleChange}
             />
-            <div className='grid md:grid-cols-2 gap-4'>
+            <div className="grid md:grid-cols-2 gap-4">
               <Input
-                type='number'
-                name='amount'
-                placeholder='Monto cambiado/solicitado'
+                type="number"
+                name="amount"
+                placeholder="Monto cambiado/solicitado"
                 value={formik.values.amount}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
@@ -176,11 +180,11 @@ const LibroReclamaciones = () => {
                 touched={formik.touched.amount}
               />
               <Select
-                name='formType'
-                placeholder='Tipo de reclamo'
+                name="formType"
+                placeholder="Tipo de reclamo"
                 options={[
-                  { value: 'Queja', label: 'Queja' },
-                  { value: 'Reclamo', label: 'Reclamo' },
+                  { value: "Queja", label: "Queja" },
+                  { value: "Reclamo", label: "Reclamo" },
                 ]}
                 value={formik.values.formType}
                 error={formik.errors.formType}
@@ -189,22 +193,22 @@ const LibroReclamaciones = () => {
               />
             </div>
             <Textarea
-              name='description'
-              placeholder='Escribe tu mensaje...'
+              name="description"
+              placeholder="Escribe tu mensaje..."
               error={formik.errors.description}
               touched={formik.touched.description}
               value={formik.values.description}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            <div className='flex items-center justify-center'>
-              <Button type='submit' disabled={isLoading || sent} className='block w-56 md:w-80'>
-                {!isLoading && !sent && 'Enviar mensaje'}
-                {isLoading && !sent && 'Enviando...'}
-                {!isLoading && sent && 'Mensaje enviado'}
+            <div className="flex items-center justify-center">
+              <Button type="submit" disabled={isLoading || sent} className="block w-56 md:w-80">
+                {!isLoading && !sent && "Enviar mensaje"}
+                {isLoading && !sent && "Enviando..."}
+                {!isLoading && sent && "Mensaje enviado"}
               </Button>
             </div>
-            {sent && <p className='sent-msg text-center mt-3'>Su mensaje ha sido recibido, pronto estaremos en contacto.</p>}
+            {sent && <p className="sent-msg text-center mt-3">Su mensaje ha sido recibido, pronto estaremos en contacto.</p>}
           </form>
         </div>
       </section>
