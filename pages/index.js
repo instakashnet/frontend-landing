00cae067b6e69f8f2a4f373bd781de0c7da73b6e
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2022-04-20 17:46:44 -0500

    whtasapp changes

--- pages/index.js
+++ pages/index.js
@@ -166,7 +166,7 @@ const Home = ({ counters }) => {
           <h2 className={styles.Title}>
             Beneficios de Instakash <br /> tu casa de cambio online
           </h2>
-          <p className={styles.Subtitle}>Disfruta de los beneficios de cambiar con nosotros..</p>
+          <p className={styles.Subtitle}>En Instakash te ayudamos a tener un mejor tipo de cambio.</p>
         </div>
         <BenefitsCarousel />
       </section>
