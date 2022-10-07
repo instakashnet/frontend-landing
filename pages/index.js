92f55b59a01058aa2a26d7fa118bcdb3cb7a4242
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2022-10-07 14:12:52 -0500

    october angamo

--- pages/index.js
+++ pages/index.js
@@ -314,7 +314,7 @@ const Home = ({ counters }) => {
         </div>
       </section>
       <BaseModal isOpen={infoModal} onClose={handleCloseModal}>
-        <Image src="/images/posters/cupon.webp" alt="Mejora el tipo de cambio con INSTAKASH usando nuestro cupón del mes." layout="fill" objectFit="contain" />
+        <Image src="/images/posters/closed.webp" alt="Mejora el tipo de cambio con INSTAKASH usando nuestro cupón del mes." layout="fill" objectFit="contain" />
         {/* <div className={styles.ModalBody}>
           <h3>Lentitud con BCP</h3>
           <p>
