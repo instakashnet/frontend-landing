2b744a0a80152a331796da70323d7ae3616eb378
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2022-12-06 13:29:36 -0500

    adding yandex and modal view fix

--- pages/_document.js
+++ pages/_document.js
@@ -39,9 +39,30 @@ class MyDocument extends Document {
               `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+           
+              ym(${process.env.NEXT_PUBLIC_YANDEX_CODE}, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });
+              `,
+            }}
+          />
           <noscript>
             <img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID_MAIN}&ev=PageView&noscript=1`} />
             <img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID_WEBTILIA}&ev=PageView&noscript=1`} />
+            <div>
+              <img src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YANDEX_CODE}`} style={{ position: "absolute", left: -9999 }} alt="" />
+            </div>
           </noscript>
           <link rel="icon" href="/favicon.ico" type="image/png" />
         </Head>
