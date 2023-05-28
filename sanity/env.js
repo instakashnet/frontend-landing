a7053b7a6b83ff9d1b4afb35644315de59f7a7b1
Roger Rengifo <rogerrengifo@Rogers-MacBook-Pro.local>
2023-05-28 18:37:12 -0500

    adding sanitiy for benefits

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2023-05-26';
 
-export const dataset = 'production';
-export const projectId = '0wo2azp3';
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = false;
