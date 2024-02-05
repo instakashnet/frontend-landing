1bd73ad8165a8930b8f78aebf0fabc7f5f85519d
Roger Rengifo <rrengifo.bairesev@gasinc.us>
2024-02-05 14:45:44 -0500

    Created the entire website page

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_DEV_API_URL,
+  baseURL: process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL,
   timeout: 10000
 });
 
