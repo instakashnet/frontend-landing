af32d49a95ada2328bfba9dd6e5be4a7f2db04de
Roger Rengifo <rrengifo.bairesev@gasinc.us>
2024-02-06 11:41:35 -0500

    changing and adding ENVS

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log({ env: process.env.NODE_ENV });
+
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL,
   timeout: 10000
